578dfd331e0556cfadcb457f7e8b27bb1e2385de
Vinicius Alves Velasco <vinicius.velasco@amaggi.com.br>
2020-01-18 23:45:14 -0400

    Ajuste na lib de transformar array em string

--- src/utils/parseStringAsArray.js
+++ src/utils/parseStringAsArray.js
@@ -1,3 +1,3 @@
 module.exports = function parseStringAsArray(arrayAsString) {
-  return arrayAsString.split(",").map(tech => tech.trim());
+  return arrayAsString ? arrayAsString.split(",").map(tech => tech.trim()) : "";
 };
